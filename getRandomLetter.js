a777a82ad2c717b63a01e48ebed71aacf8d98102
Elida Maruqyan <elida.marukyan@gmail.com>
2020-06-28 14:33:20 +0400

    add getRandomLetter function

---
+++ getRandomLetter.js
@@ -0,0 +1,15 @@
+const getRandomLetter = (str) => {
+    let result = '';
+    if (typeof str === 'string') {
+        for (let i = 0; i < 1; i++) {
+            const randomNumber = Math.floor(Math.random() * str.length);
+            result += str.charAt(randomNumber);
+        }
+
+        return result;
+    }
+
+    throw new Error("invalid value");
+};
+
+getRandomLetter(2);
